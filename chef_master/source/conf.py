4fa6b5f696093cf81df7d17e17e18e277b6d47d6
jamescott <jamescott@opscode.com>
2012-11-07 18:38:28 -0800

    correct opscode theme, try #2

--- chef_master/source/conf.py
+++ chef_master/source/conf.py
@@ -91,7 +91,7 @@ pygments_style = 'emacs'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'
+html_theme = 'opscode'
 #html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -100,7 +100,7 @@ html_theme = 'sphinxdoc'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['../../_themes/opscode']
+html_theme_path = ['../../_themes/']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
