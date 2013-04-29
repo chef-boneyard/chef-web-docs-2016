6f3bfec0fd071f17544cd582acfe3b40dd1ebca5
jamescott <jamescott@opscode.com>
2013-04-29 10:04:02 -0700

    remove previous/next from table of contents pane

--- chef_master/source/conf.py
+++ chef_master/source/conf.py
@@ -166,7 +166,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
-   '**': ['localtoc.html', 'relations.html'],
+   '**': ['localtoc.html'],
 }
 
 # Additional templates that should be rendered to pages, maps page names to
