f8c711e477f9fcbbce1bf8598aab4dfb89d37c82
jamescott <jamescott@opscode.com>
2013-12-20 10:22:45 -0800

    rebuild version string for 10.latest

--- release_chef_10/source/conf.py
+++ release_chef_10/source/conf.py
@@ -136,7 +136,7 @@ html_static_path = ['_static']
 # hack that achieved the desired behavior. Plus, there's 0% chance that we'll
 # ever want to insert a datetime stamp in the docs.
 
-html_last_updated_fmt = 'chef-client, version 10 (current)'
+html_last_updated_fmt = 'chef-client, version 10.latest'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
