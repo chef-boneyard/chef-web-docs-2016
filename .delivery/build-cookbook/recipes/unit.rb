9403034b8a17de0b8216330c031d3fbe70bc1a8e
Christopher Webber <cwebber@getchef.com>
2015-09-09 20:40:00 -0700

    This is not a ruby app, stop acting like it is

--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -10,13 +10,3 @@ include_recipe 'build-cookbook::_handler'
 # Run unit tests on the cookbooks that are in cookbooks/
 include_recipe 'delivery-truck::unit'
 
-include_recipe 'build-cookbook::_install_dependencies'
-
-execute 'run rspec' do
-  command "bundle exec rspec --format doc"
-  cwd node['delivery_builder']['repo']
-  environment({
-    "PATH" => '/opt/chefdk/embedded/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games'
-  })
-  user node['delivery_builder']['build_user']
-end
