153ee995446cb1ca17b03e797153505c4d42bcd4
Christopher Webber <cwebberops@gmail.com>
2015-09-21 10:13:36 -0700

    Add back the config variable

--- .delivery/build-cookbook/recipes/_run_builder.rb
+++ .delivery/build-cookbook/recipes/_run_builder.rb
@@ -3,6 +3,7 @@ Chef_Delivery::ClientHelper.enter_client_mode_as_delivery
 chef_aws_creds = encrypted_data_bag_item_for_environment('cia-creds', 'chef-aws')
 ENV['AWS_ACCESS_KEY_ID'] = chef_aws_creds['access_key_id']
 ENV['AWS_SECRET_ACCESS_KEY'] = chef_aws_creds['secret_access_key']
+ENV['AWS_CONFIG_FILE'] = File.join(node['delivery']['workspace']['root'], 'chef_aws_config')
 
 require 'chef/provisioning/aws_driver'
 ssh = encrypted_data_bag_item_for_environment('cia-creds', 'aws-ssh')
