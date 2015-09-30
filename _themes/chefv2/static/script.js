c648a24f36eea496d2b726f5302dcd2fc73f3ad7
Maggie Walker <magwalk@gmail.com>
2015-09-30 08:15:16 -0700

    Add Google Analytics outbound link tracking
    Adds outbound link tracking to docs.chef.io. Includes adding a function to check each anchor tag for outbound pointing links and adds GA function to the onclick attribute of each one. Read more about GA outbound link tracking here: https://support.google.com/analytics/answer/1136920?hl=en .

--- _themes/chefv2/static/script.js
+++ _themes/chefv2/static/script.js
@@ -1,4 +1,41 @@
-$(document).ready(function() { 
+var hasHref = function (linkURL) {
+  return linkURL == undefined ? true : false;
+}
+
+var isAbsoluteLink = function (linkURL) {
+  return (linkURL.indexOf("/") != 0) && (linkURL.indexOf("http") >= 0);
+}
+
+var isDifferentHostname = function (linkURL) {
+  return linkURL.indexOf(location.hostname) < 0;
+}
+
+var isFragmentLink = function(linkURL) {
+  return linkURL.indexOf("#") != 0;
+}
+
+var isOutboundLink = function(linkURL) {
+  return isAbsoluteLink(linkURL) && isDifferentHostname(linkURL) && isFragmentLink(linkURL);
+}
+
+var addOutboundLinkTracking = function() {
+  $("a").each(function() {
+    var $link = $(this);
+    var linkURL = $link.attr("href");
+
+    if (hasHref(linkURL)) {
+      return true;
+    }
+
+    if (isOutboundLink(linkURL)) {
+      $link.attr("onclick", "trackOutboundLink('" + linkURL + "'); return false;");
+    }
+  });
+};
+
+$(document).ready(function() {
+  addOutboundLinkTracking();
+
   // Nav Interactions
   const $navLinks = $(".nav-links.links-main");
   const $navDocs = $(".nav-docs");
